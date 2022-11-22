f0306afb47947cbf809d679e434ead642983c9ed
sebastian <sebastian.dorsch@4rsoluciones.com>
2022-11-21 22:38:49 -0300

    ejercicio clase 1

---
+++ Clase1.js
@@ -0,0 +1,17 @@
+let nombre = 'Sebastian'
+let edad = 29
+let precio = 30.50
+let series = ['game of thrones', 'friends', 'black mirror']
+let peliculas = ['matrix', 'her', 'exmachina']
+
+console.log(nombre)
+console.log(edad)
+console.log(precio)
+console.log(series)
+console.log(peliculas)
+
+edad += 1
+series.push('the office')
+
+console.log(edad)
+console.log(series)
